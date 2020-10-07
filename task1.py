1b399ea714e49eb7b430651eb0ddbbde509bdcc1
ccallow <71406811+ccallow@users.noreply.github.com>
2020-10-07 12:24:04 -0700

    f

--- task1.py
+++ task1.py
@@ -5,11 +5,11 @@ def sum(a,b):
     # a : float 
     # b : float
     # return value: returns the sum of the 2 numbers
+    x= a+b
+    return x
+    
+a = float(input("Enter a number: "))
+b = float(input("Enter a number: "))
 
-
-
-#this should return a value of 7
-x = sum(3,4)
-
-#this should return a value of 12.5
-y = sum(11,1.5)
+x = sum(a,b)
+print(x)

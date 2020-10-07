51ac8ffec11e854f4139faaebffba54370b054fb
ccallow <71406811+ccallow@users.noreply.github.com>
2020-10-07 12:49:18 -0700

    j

--- task3.py
+++ task3.py
@@ -6,3 +6,14 @@ The return value is the sum of all the numbers in the list
 added together
 (2 points)
 """
+
+def perimeter(myList):
+    #inputs
+    #list = listNumbers
+    #return is sum of numbers in listNumbers
+    x = sum(myList)
+    return x
+
+a = [3, 6, 9, 6]
+b = perimeter(a)
+print(b)
--- task4.py
+++ task4.py
@@ -6,4 +6,17 @@ Return True if the number is an integer
 Return False if the number is not an integer
 (2 points)
 """
+def isInteger(x):
+    #inputs
+    #x = float
+    #return True if a is an integer
+    #return False if a is not an integer
+    if x == int(x):
+        return True
+    elif x != int(x):
+        return False
+    
 
+a = float(input("Enter a number: "))
+b = isInteger(a)
+print(b)

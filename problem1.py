e05484b0df3def61c6fcfd83b55c6baa8b35df44
ccallow <71406811+ccallow@users.noreply.github.com>
2020-10-08 11:03:33 -0700

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -2,8 +2,19 @@
 """ 
 Create a function called hypotenuse()
 Input is 2 float numbers and a boolean
-If the boolean is True, then find the hypotenuse
-If the boolean is False, then the larger number is the hypotenuse
+If the boolean is False, then find the hypotenuse
+If the boolean is True, then the larger number is the hypotenuse
 Return the missing side
 (2 points)
 """
+def hypotenuse(x):
+    #inputs
+    #a = float
+    #b = float
+    # x =True or False
+    import math 
+    if True:
+        x = math.sqrt((a**2) + (b**2))
+        return x
+    if False:
+        x = math(a**2)

11655899d1cb9f4172558b51eef3e0d42e72995e
ccallow <71406811+ccallow@users.noreply.github.com>
2020-10-08 12:24:11 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -23,6 +23,8 @@ def distance(coor1,coor2):
     distance=float((x**2 + y**2)**(1/2))
     distance = round(distance,3)
     return distance
+
+    
 coor1=[]
 coor2=[]
 a=int(input("Enter x value: "))
